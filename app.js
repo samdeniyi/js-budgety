2293df1da19cbd9f80b6537cde9e3e04062e0da3
Bigsam <sam.adeniyi@appliso.com>
2018-02-01 06:06:39 +0100

    building the budgetController

--- app.js
+++ app.js
@@ -1,8 +1,52 @@
 // BUDGET CONTROLLER
 var budgetController = (function () {
+    var Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
 
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var data = {
+        allItems:{
+            exp:[],
+            inc:[]
+        },
+        totals:{
+            exp:0,
+            inc:0
+        }
+    }
+
+    return{
+        addItem: function (type, des, val) {
+            var newItem, ID;
+
+            if(data.allItems[type].length == 0){
+                ID = 0
+            }else{
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            }
+
+            if(type === 'exp'){
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+
+            data.allItems[type].push(newItem);
+            return newItem;
+        },
+        testing : function () {
+            console.log(data.allItems);
+        }
+    }
 })();
-
 
 // UI CONTROLLER
 var UIController = (function () {
@@ -30,36 +74,46 @@ var UIController = (function () {
 
 //GLOBAL APP CONTROLLER
 var appController = (function (budgetCtrl, UICtrl) {
-    var DOM = UICtrl.getDOMstrings();
+    var setupEventListeners = function(){
+        var DOM = UICtrl.getDOMstrings();
+        document.querySelector(DOM.addBtn).addEventListener('click', appCtrlAddItem);
+        document.addEventListener('keypress', function (ev) {
+            if(ev.keyCode === 13 || ev.which === 13){
+                appCtrlAddItem();
+            }
+        });
+    };
     var appCtrlAddItem = function () {
+        var input, newItem;
         // 1. Get the filled input data
-        var input = UICtrl.getInput();
-        console.log(input);
+        input = UICtrl.getInput();
         // 2. Add the item to the budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         // 3. Add the item to the UI
         // 4. Cal budget
         // 5. Display the budget on the UI
-    }
-    document.querySelector(DOM.addBtn).addEventListener('click', appCtrlAddItem);
-
-    document.addEventListener('keypress', function (ev) {
-        if(ev.keyCode === 13 || ev.which === 13){
-            appCtrlAddItem();
-        }
-    });
-
-})(budgetController,UIController);
-
-(function () {
-    var Person = function (name, phone) {
-        this.name = name;
-        this.phone = phone;
+        budgetCtrl.testing();
     };
 
-    Person.prototype.career = function (career) {
-        console.log(this.name+" is a "+career+" guy");
+    return {
+        init: function () {
+            console.log('init', 'Application has started');
+            setupEventListeners();
+        }
     }
 
-    var sam = new Person('Sam', '08030600957');
-    sam.career('UX');
+})(budgetController,UIController);
+appController.init();
+(function () {
+    // var Person = function (name, phone) {
+    //     this.name = name;
+    //     this.phone = phone;
+    // };
+
+    // Person.prototype.career = function (career) {
+    //     console.log(this.name+" is a "+career+" guy");
+    // }
+
+    // var sam = new Person('Sam', '08030600957');
+    // sam.career('UX');
 })();
